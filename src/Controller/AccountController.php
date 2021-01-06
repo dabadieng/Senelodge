90a451f7d80c163fb063f7a383dfd31bcd1d35e0
dabadieng <58230519+dabadieng@users.noreply.github.com>
2021-01-06 16:08:57 +0100

    finalisation du module réservation

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -163,4 +163,15 @@ class AccountController extends AbstractController
             "user" => $this->getUser()
         ]);
     }
+
+    /**
+     * Permet d'afficher les réservations faites par l'utilisateur
+     * @Route("/account/bookings", name="account_booking")
+     *
+     * @return Response 
+     */
+    public  function bookings()
+    {
+        return $this->render("account/bookings.html.twig");
+    }
 }
