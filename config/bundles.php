4217b56c57186f53cc75f3e664ff2605d3086565
dabadieng <58230519+dabadieng@users.noreply.github.com>
2021-02-22 16:44:32 +0100

    pagination

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
